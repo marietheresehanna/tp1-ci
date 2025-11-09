01d43d92a9c74c58e74c2ad6ab12088f97b231ff
marietheresehanna <marie-therese.hanna@net.usj.edu.lb>
2025-11-09 20:27:45 +0200

    Create test_main.py

---
+++ src/tests/test_main.py
@@ -0,0 +1,10 @@
+# tests/test_main.py
+import unittest
+from src.main import add
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
